511e85a0f9ec0fb70e159a23dd2462b124622aa2
John P. Giatropoulos <john.giatropoulos@gmail.com>
2024-04-04 10:20:30 -0400

    main, updated ResizeObserver bug in CodeEditorVS

--- src/Common/CodeEditor/CodeEditorVS.js
+++ src/Common/CodeEditor/CodeEditorVS.js
@@ -5,6 +5,30 @@ import { getStylesForItem, themeObjects } from "@dash/Utils";
 
 const parseTmTheme = require("monaco-themes").parseTmTheme;
 
+// Save a reference to the original ResizeObserver
+const OriginalResizeObserver = window.ResizeObserver;
+
+// Create a new ResizeObserver constructor
+window.ResizeObserver = function (callback) {
+    const wrappedCallback = (entries, observer) => {
+        window.requestAnimationFrame(() => {
+            callback(entries, observer);
+        });
+    };
+
+    // Create an instance of the original ResizeObserver
+    // with the wrapped callback
+    return new OriginalResizeObserver(wrappedCallback);
+};
+
+// Copy over static methods, if any
+for (let staticMethod in OriginalResizeObserver) {
+    if (OriginalResizeObserver.hasOwnProperty(staticMethod)) {
+        window.ResizeObserver[staticMethod] =
+            OriginalResizeObserver[staticMethod];
+    }
+}
+
 export function CodeEditorVS({
     code,
     onChange,
